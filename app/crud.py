cba018f80576a1082e596bcd7978633f079d1ffd
amirmortezaee1 <amir.mortezaee1@gmail.com>
2022-12-21 17:00:35 +0330

    fixe naming convention

--- app/crud.py
+++ app/crud.py
@@ -10,18 +10,19 @@ class Product:
                  sale_price,
                  manage_stoke,
                  stoke_quantity,
-                 isAvailable,
-                 isVisible
+                 is_available,
+                 is_visible
                  ):
         self.title = title
         self.short_description = short_description
         self.description = description
         self.slug = slug
         self.permalink = permalink
-        self.isAvailable = isAvailable
+        self.isAvailable = is_available
         self.price = price
         self.regular_price = regular_price
         self.sale_price = sale_price
         self.manage_stoke = manage_stoke
         self.stoke_quantity = stoke_quantity
-        self.isVisible = isVisible
+        self.is_visible = is_visible
+        
